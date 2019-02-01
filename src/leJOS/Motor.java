2989792aec3d6d02ec56219aefd96b8f66ced912
Benjamin <benjamin.longa@gmail.com>
2019-02-01 11:27:42 +0100

    Test Pince, Navigator, PosPalets

---
+++ src/leJOS/Motor.java
@@ -0,0 +1,33 @@
+package leJOS;
+
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.utility.Delay;
+
+
+public class Motor
+{
+    public static void main(String[] args)
+    {
+        log("Program Starting");
+
+        EV3LargeRegulatedMotor motor = new EV3LargeRegulatedMotor(MotorPort.B);
+        motor.setSpeed(500);
+
+        log("Forward motion");
+        motor.forward();
+
+        Delay.msDelay(3000);
+
+        log("Stopping motor");
+        motor.stop();
+
+        log("Program Ending");
+    }
+
+
+    private static void log(String msg) 
+    { 
+        System.out.println("log>\t" + msg); 
+    }
+}
