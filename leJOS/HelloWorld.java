b27729986367191c7ecd5c3f851900f8f629c29f
rookah <gisselairel@gmail.com>
2019-02-01 10:14:52 +0100

    add HelloWorld

---
+++ leJOS/HelloWorld.java
@@ -0,0 +1,11 @@
+package leJOS;
+
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		LCD.drawString("Plugin Test", 0, 4);
+		Delay.msDelay(5000);
+	}
+}
