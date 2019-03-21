48a61e6ef0a7df8352839f8acf49ab1f1cb84185
Nadia Benmoussa <nadia.benmoussa@etu.univ-grenoble-alpes.fr>
2019-03-21 21:09:20 +0100

    des coordonnees +  des lettres + intervalles

---
+++ src/tests/Point.java
@@ -0,0 +1,36 @@
+package tests;
+
+public class Point {
+	String id; 
+	float coord1; 
+	float coord2; 
+	public Point(String id, float coord1, float coord2) {
+		super();
+		this.id = id;
+		this.coord1 = coord1;
+		this.coord2 = coord2;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public float getCoord1() {
+		return coord1;
+	}
+	public void setCoord1(float coord1) {
+		this.coord1 = coord1;
+	}
+	public float getCoord2() {
+		return coord2;
+	}
+	public void setCoord2(float coord2) {
+		this.coord2 = coord2;
+	}
+
+	void valeurDansListe(float a, float b)
+	{
+		
+	}
+}
